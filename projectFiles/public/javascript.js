d88bee32e0b700ea4fb550a3e1b3ea328a232c28
Mohammad <mohammadtarekhassan@gmail.com>
2018-05-10 22:56:13 +0200

    added date and time to javascript alert

--- projectFiles/public/javascript.js
+++ projectFiles/public/javascript.js
@@ -1,8 +1,17 @@
 function clear(){
-    document.getElementById('commandValue').clear;
-    document.getElementById('command').clear;
+    document.getElementById('commandValue').value="";
+    document.getElementById('command').value="";
 };
 
+//Time and Date
+var currentdate = new Date(); 
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " - "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
 function commandAlert(){
-    alert("Command: " + document.getElementById('command').value + " with value " + document.getElementById('commandValue').value + " has been sent to the Drone. Time: ");
+    alert("Command: " + document.getElementById('command').value + " with value " + document.getElementById('commandValue').value + " has been sent to the Drone at: " + datetime);
 }
