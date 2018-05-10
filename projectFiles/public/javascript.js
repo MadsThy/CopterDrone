3292e7dbfc09a6f004f36bbb4334b68cb3fa0421
Mohammad <mohammadtarekhassan@gmail.com>
2018-05-10 22:47:49 +0200

    added JavaScript alert for command

--- projectFiles/public/javascript.js
+++ projectFiles/public/javascript.js
@@ -3,3 +3,6 @@ function clear(){
     document.getElementById('command').clear;
 };
 
+function commandAlert(){
+    alert("Command: " + document.getElementById('command') + " with value " + document.getElementById('commandValue') + " has been sent to the Drone. Time: ");
+}
