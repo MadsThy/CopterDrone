65f177742162de27a74a000d849eec3b6c613d81
pixel321mohammad <mohammadtarekhassan@gmail.com>
2018-05-15 15:27:54 +0200

    made parantheses around print outs so python 3 also runs

--- projectFiles/test.py
+++ projectFiles/test.py
@@ -3,22 +3,22 @@ from dronekit import connect, VehicleMode
 from time import sleep
 
 # Connect to the Vehicle.
-print "Connecting to drone"
+print ("Connecting to drone")
 vehicle = connect('udp:127.0.0.1:14550')
 
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name    # settable
-print "######################################"
-print "#    Arming drone. Stand clear!!!    #"
-print "######################################"
+print ("System status: %s" % vehicle.system_status.state)
+print ("Mode: %s" % vehicle.mode.name)
+print ("######################################")
+print ("#    Arming drone. Stand clear!!!    #")
+print ("######################################")
 sleep(2)
 vehicle.armed = True
-print "Disarming in..."
-print "3..."
+print ("Disarming in...")
+print ("3...")
 sleep(1)
-print "2..."
+print ("2...")
 sleep(1)
-print "1..."
+print ("1...")
 sleep(1)
 vehicle.armed = False
 
