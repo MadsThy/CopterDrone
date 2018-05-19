c1287d75d42a640cd81a946f233d00a252523af4
Mads Thy <mads@heimdal-thy.dk>
2018-05-19 15:21:29 +0200

    Testing Python as a service

--- projectFiles/takeoff.py
+++ projectFiles/takeoff.py
@@ -18,6 +18,8 @@ print "######################################"
 print "#    Arming drone. Stand clear!!!    #"
 print "######################################"
 
+##########################################################################################
+
 def turn(heading, relative, direction):
     print "Current Heading: %s" % vehicle.heading
         
@@ -60,45 +62,27 @@ def turn(heading, relative, direction):
             break
         sleep(0.5)
 
+##########################################################################################
 
 def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-
-    print "Basic pre-arm checks"
-    # Don't try to arm until autopilot is ready
-    #while not vehicle.is_armable:
-    #    print " Waiting for vehicle to initialise..."
-    #    sleep(1)
-    #
-    #    print "Arming motors"
-    # Copter should arm in GUIDED mode
     vehicle.mode    = VehicleMode("GUIDED")
     print " Waiting for arming..."
     sleep(13)
-
-
-    # Confirm vehicle armed before attempting to take off
     vehicle.armed   = True
 
     sleep(2)
     print "Taking off!"
     vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
     while True:
         print " Altitude: ", vehicle.location.global_relative_frame.alt
         #Break and return from function just below target altitude.
         if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
             print "Reached target altitude"
-            #vehicle.mode    = VehicleMode("STABILIZE")
-            #sleep(1)
-            #vehicle.armed   = False
             break
         sleep(0.5)
 
+##########################################################################################
 
 arm_and_takeoff(vehicle.location.global_relative_frame.alt+1) # Fly up 1 meter relative to current altitude.
 
