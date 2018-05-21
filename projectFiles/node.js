d7af59e0363d05fd16417a52dbf942d79d4ad3d7
Mads Thy <mads@heimdal-thy.dk>
2018-05-22 00:11:48 +0200

    New version of website with easy commands

--- projectFiles/node.js
+++ projectFiles/node.js
@@ -36,69 +36,32 @@ app.get('/', function (req, res) {
 //When receiving simple commands, do this to commands sent via x.x.x.x/commands?command=xxx
 app.get('/commands', function (req, res) {
     commandString = 'python command.py '+req.param('command');
-    var pyProcess = cmd.get(commandString);
-    console.log("Command sent: "+commandString);
-
-    res.end("OK"); //Something can be put here, if you want to return some data to the browser
-
 
     //Create eventhandler
     var eventEmitter = new events.EventEmitter();
 
     //Append date, time, and command to the logging file
     var myEventHandler = function () {
-        fs.appendFile('usercommands.txt', "Command: " + usercommand + " sent at: " + new Date().toISOString().
-                      replace(/T/, ' '). // replace T with a space
-                      replace(/\..+/, '') + "\r\n", function (err) {
-            if (err) throw err;
-            console.log('[INFO] Command logged to file.');
-        });
-        //Assign the event handler to an event. "savefile" is the name of the event
-        eventEmitter.on('savefile', myEventHandler);
-        eventEmitter.emit('savefile');
-    }
+        var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+        fs.appendFile('usercommands.txt', date+" - command '" + req.param('command') + "' sent\r\n");
+        console.log('[INFO] Command logged to file.');
 
-    });
+    };
+    eventEmitter.on('savefile', myEventHandler);
+    eventEmitter.emit('savefile');
+
+    var pyProcess = cmd.get(commandString);
+    console.log("Command sent: "+commandString);
+
+    res.end("OK"); //Something can be put here, if you want to return some data to the browser
+
+});
 
 //--------------------------------------------------
 //AJAX call from /command. This sends a command to the drone by calling the scripts.py Python script. This also logs the command into a textfile stored locally.
 app.post('/commands', function (req, res) {
 
     res.end("No POST commands ready yet."); //Something can be put here, if you want to return some data to the browser
-});
-
-//--------------------------------------------------
-//AJAX call from /dronestatus. This reads the status of the Drone from the droneinfo.py Python script, and returns the value inside the HTML dronestatus textarea
-app.get('/dronestatus', function (req, res) {
-
-    console.log("Trying to get dronestatus");
-    //Call python script and get a return value from the "data" parameter
-    var process = spawn('python', ["droneinfo.py"]);
-    process.stdout.on('data', function (data) {
-        console.log(data.toString());
-
-        //Create eventhandler
-        var eventEmitter = new events.EventEmitter();
-
-        //Append date, time, and droneinfo to a file
-        var myEventHandler = function () {
-            fs.appendFile('droneinfo.txt', data + " at: " + new Date().toISOString().
-                          replace(/T/, ' '). // replace T with a space
-                          replace(/\..+/, '') + "\r\n", function (err) {
-                if (err) throw err;
-                console.log('[INFO] File logged to system.');
-            });
-        }
-
-        //Assign the event handler to an event:
-        eventEmitter.on('savefile', myEventHandler);
-
-        //Fire the 'savefile' event:
-        eventEmitter.emit('savefile');
-
-        //Send end data back to HTML textarea
-        res.end(data);
-    });
 });
 
 //--------------------------------------------------
